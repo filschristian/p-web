62611d5c904beb07d33680d9a2cc1b9da918cc03
filschristian <filschristian94@gmail.com>
2020-01-29 13:17:00 +0200

    home page design and implementation

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toggle_nav() {
+  var x = document.getElementById("navigation-bar");
+  if (x.className === ""){
+    x.className = "responsive";
+  }else {
+    x.className = "";
+  }
+  
+}
